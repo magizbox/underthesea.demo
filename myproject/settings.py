45408d09eb443bff144add97a8cc9404386d0e6f
rain <brother.rain.1024@gmail.com>
2017-08-21 15:02:39 +0700

    update service

--- myproject/settings.py
+++ myproject/settings.py
@@ -113,7 +113,8 @@ DATABASES['default'] = dj_database_url.config(conn_max_age=500)
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
-# Allow all host headersALLOWED_HOSTS = ['*']
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
