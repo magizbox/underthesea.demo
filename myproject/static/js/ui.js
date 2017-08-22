fd54308b4d32082fea9f70a0beda85b9a461d750
rain <brother.rain.1024@gmail.com>
2017-08-22 11:34:22 +0700

    update service

--- myproject/static/js/ui.js
+++ myproject/static/js/ui.js
@@ -44,7 +44,7 @@ function flashLabel(){
     var time = 400;
     for(var i=0; i<1; i++){
         $("#live-label").animate({"color": "#fff"}, time);
-        $("#live-label").animate({"color": "#c0dbab"}, time);
+        $("#live-label").animate({"color": "#a2daed"}, time);
     }
     $("#live-label").animate({"color": "#fff"}, time);
 }
