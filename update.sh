6013e47e65e5ecfe4db5d1b26e85e024c44b29bc
rain <brother.rain.1024@gmail.com>
2017-10-06 14:41:30 +0700

    automatic deployment

--- update.sh
+++ update.sh
@@ -1,1 +1,3 @@
-git pull origin demo.beta
+git pull origin beta
+pip uninstall -y underthesea; pip install git+https://github.com/magizbox/underthesea.git@beta
+python manage.py runserver 0.0.0.0:9001
