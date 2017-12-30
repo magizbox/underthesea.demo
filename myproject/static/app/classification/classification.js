615d711a1fd224459950eab94441b3ad0e24b54b
rain <brother.rain.1024@gmail.com>
2017-12-30 09:50:12 +0700

    update classification

--- myproject/static/app/classification/classification.js
+++ myproject/static/app/classification/classification.js
@@ -1,16 +1,27 @@
 app.controller("ClassificationCtrl", function ($scope) {
     $scope.texts = {
-        "general": ["Hành khách la hét trên chiếc xe buýt gặp nạn, 4 người nhập viện",
-        "Người hút thuốc lá nên ăn táo, cà chua mỗi ngày",
-        "Facebook gửi thông báo khi ảnh người dùng bị đăng tải lên mạng"],
-        "bank": ["Tôi rất thích cách phục vụ của nhân viên BIDV"]
+        "general": [
+            "Hành khách la hét trên chiếc xe buýt gặp nạn, 4 người nhập viện",
+            "Người hút thuốc lá nên ăn táo, cà chua mỗi ngày",
+            "Facebook gửi thông báo khi ảnh người dùng bị đăng tải lên mạng"
+        ],
+        "bank": [
+            "Tôi rất thích cách phục vụ của nhân viên BIDV",
+            "Ad ơi! Hiện giờ lãi xuất gửi tiết kiệm 3 tháng là nhiu % z ad?? ",
+            "tôi chuyển tiền sang viettinbank không thành công mà vẫn mất phí 7.7K nè. "
+        ]
     };
     $scope.samples = $scope.texts["general"];
     $scope.domains = ["general", "bank"];
     $scope.domain = "general";
     $scope.text = "Việt Nam hạ Indonesia 3-0, rộng cửa vào bán kết U18 Đông Nam Á. Cả Việt Nam lẫn Indonesia đang có hai chiến thắng, và chia nhau hai vị trí dẫn đầu bảng B. Vì thế, cuộc đối đầu giữa hai đội được xem như trận quyết định đến ngôi đầu bảng.";
+    $scope.updateText = function (text) {
+        $scope.text = text;
+        $scope.tag = "";
+    };
     $scope.$watch("domain", function (domain) {
-        $scope.text = $scope.texts[domain];
+        $scope.samples = $scope.texts[domain];
+        $scope.text = $scope.samples[0];
         $scope.tag = "";
     });
     var generateOutput = function () {
